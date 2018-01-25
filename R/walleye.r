17b35f64da012b06c452a61c831800e13275607e
Steven H. Ranney <steven.ranney@gmail.com>
2018-01-25 09:43:06 -0700

    More refactor.

--- R/walleye.r
+++ R/walleye.r
@@ -165,66 +165,33 @@ sink()
 normality_tests <- 
   wae %>%
   group_by(psd, lake) %>%
-  summarise(shapiro_test_pvalue = ifelse(length(rel_weight) < 3, NA, (rel_weight %>% shapiro.test())$p.value)) %>%
+  summarise(shapiro_test_pvalue = ifelse(length(rel_weight) < 3, NA, (rel_weight %>% shapiro.test())$p.value), 
+            sample_sizes = sum(!is.na(rel_weight))) %>%
   arrange(lake, psd)
 
 
-#Another method to calculate Coeffecient of determination
-#SST <- sum((wae.vol.length$vol-mean(predict(max.vol)))^2)
-#SSE <- sum((wae.vol.length$vol-(predict(max.vol)))^2)
-#1-(SSE/SST)
-
-
-
-#Percent difference between Wr and WrS
-((tapply(WrE, psd, median)-tapply(Wr, psd, median))/tapply(Wr, psd, median))*100
-
-#Sample sizes
-tapply(wae$rel_weight, wae$psd, FUN = function(x) sum(!is.na(x)))
-
-shapiro.test(wae$rel_weight)
+# #Percent difference between Wr and WrS
+# ((tapply(WrE, psd, median)-tapply(Wr, psd, median))/tapply(Wr, psd, median))*100
+# 
+# #Sample sizes
+# tapply(wae$rel_weight, wae$psd, FUN = function(x) sum(!is.na(x)))
+# 
+# shapiro.test(wae$rel_weight)
+# 
+# wae %>%
+#   group_by(psd) %>%
+#   wilcox.test(rel_weight~rel_weight_empty, data = .)
 
 wae %>%
   group_by(psd) %>%
-  wilcox.test(rel_weight~rel_weight_empty, data = .)
+  summarise(mw_wr_wre = (wilcox.test(rel_weight, y = rel_weight_empty)$p.value), 
+            mw_wre_wrm = (wilcox.test(rel_weight_empty, y = rel_weight_max_nls)$p.value), 
+            mw_wr_wrm = (wilcox.test(rel_weight, y = rel_weight_max_nls)$p.value))
 
 
-
-74with(wae[psd=="1",],
-  wilcox.test(Wr, y=WrE))
-with(wae[psd=="2",],
-  wilcox.test(Wr, y=WrE))
-with(wae[psd=="3",],
-  wilcox.test(Wr, y=WrE))
-with(wae[psd=="4",],
-  wilcox.test(Wr, y=WrE))
-
-#Comparing WrE to WrMAX
-with(wae[psd=="0",],
-    wilcox.test(WrE, y=Wr.max))
-with(wae[psd=="1",],
-    wilcox.test(WrE, y=Wr.max))
-with(wae[psd=="2",],
-    wilcox.test(WrE, y=Wr.max))
-with(wae[psd=="3",],
-  wilcox.test(WrE, y=Wr.max))
-with(wae[psd=="4",],
-  wilcox.test(WrE, y=Wr.max))
 
 #Percent difference between WrE and Wr.max
 ((tapply(WrE, psd, median)-tapply(Wr.max, psd, median))/tapply(Wr.max, psd, median))*100
-
-#Comparing Wr to WrMAX
-with(wae[psd=="0",],
-    wilcox.test(Wr, y=Wr.max))
-with(wae[psd=="1",],
-    wilcox.test(Wr, y=Wr.max))
-with(wae[psd=="2",],
-    wilcox.test(Wr, y=Wr.max))
-with(wae[psd=="3",],
-    wilcox.test(Wr, y=Wr.max))
-with(wae[psd=="4",],
-    wilcox.test(Wr, y=Wr.max))
 
 #Percent difference between Wr and Wr.max
 ((tapply(Wr, psd, median)-tapply(Wr.max, psd, median))/tapply(Wr.max, psd, median))*100
@@ -274,30 +241,3 @@ errbar((c(1.5:3.5, 5.5:7.5, 9.5:11.5, 13.5:15.5, 17.5:19.5)),
         z.q.u.wae,
         z.q.l.wae,
         add=T, pch=" ")
-
-
-#detach(wae)
-#rm(list=ls(all=TRUE))
-
-#Least-squares regression on log10-transformed data
-#wae.vol.length <- read.table("wae.vol.length.txt")
-#mod.1 <- lm(log10(wae.vol.length$vol)~log10(wae.vol.length$Length))
-#plot(log10(wae.vol.length$vol)~log10(wae.vol.length$Length))
-#  abline(mod.1)
-#summary(mod.1)
-
-#dffits(mod.1)
-#length(wae.vol.length$Length)
-#2*(sqrt(3/29))
-#wae.vol.length <- cbind(wae.vol.length, abs(dffits(mod.1)))
-#length.1 <- ifelse(abs(dffits(mod.1))>0.6432675, NA, wae.vol.length$Length)
-#wae.vol.length <- cbind(wae.vol.length, length.1)
-#vol.1 <- ifelse(length.1>0, wae.vol.length$vol, NA)
-#wae.vol.length <- cbind(wae.vol.length, vol.1)
-
-#mod.2 <- lm(log10(wae.vol.length$vol.1)~log10(wae.vol.length$length.1))
-#plot(log10(wae.vol.length$vol.1)~log10(wae.vol.length$length.1))
-#  abline(mod.2)
-#  abline(mod.1, col="red")
-#summary(mod.2)
-
