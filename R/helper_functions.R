4d1e5c98d0a397eea2a1672cbac7a5513c2a137a
Steven H. Ranney <steven.ranney@gmail.com>
2018-01-29 08:00:49 -0700

    Change .doc name. Add helper functions.

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,11 +1,36 @@
 
 #Calculate Walleye relative weight with standard weight (Ws) equation
-
-calc_wae_wr <- function(weight, length){
+alc_wae_wr <- function(weight, length){
   
   (weight/(10^(-5.453+3.180*(log10(length)))))*100
 
 }
+
+#Calculate smallmouth relative weight with standard weight (Ws) equation
+calc_smb_wr <- function(weight, length){
+  
+  (weight/(10^(-5.329+3.2*(log10(length)))))*100
+  
+}
+
+assign_wae_psd <- function(data){
+  
+  ifelse((data>=250)&(data<380), "S-Q",
+         ifelse((data>=380)&(data<510), "Q-P",
+                ifelse((data>=510)&(data<630), "P-M",
+                       ifelse((data>=630)&(data<760), "M-T",
+                              ifelse(data>=760, ">T", "Substock")))))
+}
+
+assign_smb_psd <- function(data){
+  
+  ifelse((data>=180)&(data<280), "S-Q",
+                        ifelse((data>=280)&(data<350), "Q-P",
+                               ifelse((data>=350)&(data<430), "P-M",
+                                      ifelse((data>=430)&(data<510), "M-T",
+                                             ifelse(data>=510, ">T", "Substock")))))
+}
+
 
 # Helper for assinging length classes
 
